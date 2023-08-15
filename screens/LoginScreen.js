47b93487ee44f08cd0450417d33b641b65232707
Marzooq13579 <marzooqabs@gmail.com>
2023-08-15 17:17:52 +0530

    login screeen design finished

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -4,11 +4,16 @@ import {
   View,
   Image,
   KeyboardAvoidingView,
+  TextInput,
+  Pressable,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { MaterialIcons, AntDesign } from "@expo/vector-icons";
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <SafeAreaView
       style={{ flex: 1, backgroundColor: "white", alignItems: "center" }}
@@ -22,7 +27,7 @@ const LoginScreen = () => {
         />
       </View>
       <KeyboardAvoidingView>
-        <View>
+        <View style={{ alignItems: "center" }}>
           <Text
             style={{
               fontSize: 17,
@@ -33,6 +38,103 @@ const LoginScreen = () => {
           >
             Login to your Account
           </Text>
+        </View>
+        <View style={{ marginTop: 70 }}>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 5,
+              backgroundColor: "#D0D0D0",
+              paddingVertical: 5,
+              borderRadius: 5,
+              marginTop: 30,
+            }}
+          >
+            <MaterialIcons
+              style={{ marginLeft: 8 }}
+              name="email"
+              size={24}
+              color="gray"
+            />
+            <TextInput
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+              style={{
+                color: "gray",
+                marginVertical: 10,
+                width: 300,
+                fontSize: email ? 16 : 16,
+              }}
+              placeholder="Enter your Email"
+            />
+          </View>
+
+          <View style={{ marginTop: 10 }}>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 5,
+                backgroundColor: "#D0D0D0",
+                paddingVertical: 5,
+                borderRadius: 5,
+                marginTop: 30,
+              }}
+            >
+              <AntDesign
+                style={{ marginLeft: 8 }}
+                name="lock1"
+                size={24}
+                color="gray"
+              />
+              <TextInput
+                value={password}
+                onChangeText={(text) => setPassword(text)}
+                secureTextEntry={true}
+                style={{
+                  color: "gray",
+                  marginVertical: 10,
+                  width: 300,
+                  fontSize: password ? 16 : 16,
+                }}
+                placeholder="Enter your Password"
+              />
+            </View>
+          </View>
+
+          <View
+            style={{
+              marginTop: 12,
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Text>Keep me Logged In</Text>
+            <Text style={{ color: "#007FFF", fontWeight: 500 }}>
+              Forgot Password
+            </Text>
+          </View>
+
+          <View style={{ marginTop: 80 }} />
+
+          <Pressable
+            style={{
+              width: 200,
+              backgroundColor: "#FEBE10",
+              borderRadius: 6,
+              marginLeft: "auto",
+              marginRight: "auto",
+              padding: 15,
+            }}
+          >
+            <Text style={{textAlign:"center",color:"white",fontSize:16,fontWeight:"bold"}}>Login</Text>
+          </Pressable>
+
+          <Pressable style={{marginTop:15}}>
+            <Text style={{textAlign:"center",color:"gray",fontSize:16}}>Dont have an account? Sign Up</Text>
+          </Pressable>
         </View>
       </KeyboardAvoidingView>
     </SafeAreaView>
