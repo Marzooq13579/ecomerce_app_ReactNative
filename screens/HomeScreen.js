93537cba0415931f4fc4824df302e84471809783
Marzooq13579 <marzooqabs@gmail.com>
2023-08-20 20:01:51 +0530

    created searchbar in homescreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,81 @@
-import { StyleSheet, Text, View,SafeAreaView, Platform } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  ScrollView,
+  Pressable,
+  TextInput,
+} from "react-native";
+import React from "react";
+import { Feather } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView style={{padding : Platform.OS == "android" ? 40: 0,flex:1,backgrundColor:"white"}}>
-      <Text>HomeScreen</Text>
+    <SafeAreaView
+      style={{
+        padding: Platform.OS == "android" ? 40 : 0,
+        flex: 1,
+        backgrundColor: "white",
+      }}
+    >
+      <ScrollView>
+        <View
+          style={{
+            backgroundColor: "#00CED1",
+            padding: 10,
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <Pressable
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginHorizontal: 7,
+              gap: 10,
+              backgroundColor: "white",
+              borderRadius: 3,
+              height: 38,
+              flex: 1,
+            }}
+          >
+            <AntDesign
+              style={{ paddingLeft: 10 }}
+              name="search1"
+              size={22}
+              color="black"
+            />
+            <TextInput placeholder="Search Amazon.in" />
+          </Pressable>
+
+          <Feather name="mic" size={24} color="black" />
+        </View>
+
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 5,
+            padding: 10,
+            backgroundColor: "#AFEEEE",
+          }}
+        >
+          <Ionicons name="location-outline" size={24} color="black" />
+          <Pressable>
+            <Text style={{fontWeight:"500",fontSize:13}}>Deliver to this location</Text>
+          </Pressable>
+        </View>
+      </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
